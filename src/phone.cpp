fdec2258f7966d35678fd907f9595b4ceba86652
Simon <zsework1@gmail.com>
2025-06-23 19:55:29 +0000

    50%

--- src/phone.cpp
+++ src/phone.cpp
@@ -1,38 +1,79 @@
 #include "phone.h"
 
+#include <algorithm>  // for std::ranges::transform
+#include <cctype>     // for std::toupper, std::tolower, std::isalpha
 #include <iostream>
+#include <map>
 
 class Phone {
  public:
+  void Add() {}
+  void Call() {}
+  void Sms() {}
+  void Exit() {}
+
+ private:
+  std::map<PhoneNumber, std::string> contacts_;
 };
 
-class Contact {};
+class Contact {
+ public:
+  void set(const std::string& name, const PhoneNumber& phoneNumber) {
+    if (!checkName(name)) {
+      std::cout << "Incorrect symbol\n";
+    } else {
+      name_ = name;
+      name_[0] = static_cast<char>(std::toupper(name_[0]));
+      if (name_.size() > 1) {
+        std::ranges::transform(name_.begin() + 1, name_.end(),
+                               name_.begin() + 1, ::tolower);
+      }
+    }
+    phoneNumber_ = phoneNumber;
+  }
+  void get() {}
+
+ private:
+  std::string name_;
+  PhoneNumber phoneNumber_;
+  bool checkName(const std::string& name) {
+    for (const auto x : name) {
+      if (!std::isalpha(static_cast<unsigned char>(x)))
+        return false;  // a check for Latin alphabet characters using <cctype>
+    }
+    return true;
+  }
+};
 
 class PhoneNumber {
  public:
-  PhoneNumber(std::string& phoneNumber) {
-    if (checkPhoneNumber(phoneNumber) == ErrorCode::Accept) {
-      number_ = phoneNumber;
-    } else if (checkPhoneNumber(phoneNumber) == ErrorCode::Length) {
+  PhoneNumber(const std::string& phoneNumber) {
+    if (checkPhoneNumber(phoneNumber) == ErrorCode::Length) {
       std::cout << "Incorrect length\n";
-    } else if (checkPhoneNumber(phoneNumber) == ErrorCode::NoPlus) {
-      std::cout << "Plus\n"; // + should be automaticaly !!!!!!!!!!!!!!!
-    }
+    } else if (checkPhoneNumber(phoneNumber) == ErrorCode::NoNumber) {
+      std::cout << "Incorrect symbol\n";
+    } else
+      number_ = "+7" + phoneNumber;
+  }
+
+  // Required for using PhoneNumber as a key in std::map (strict weak ordering)
+  bool operator<(const PhoneNumber& other) const {
+    return number_ < other.number_;
   }
 
  private:
   std::string number_{};
   enum class ErrorCode {
     Length,
-    NoPlus,
     NoNumber,
     Accept,
   };
-  ErrorCode checkPhoneNumber(std::string& phoneNumber) {
-    if (phoneNumber.size() != 12) return ErrorCode::Length;
-    if (phoneNumber.front() != '+') return ErrorCode::NoPlus;
+  ErrorCode checkPhoneNumber(const std::string& phoneNumber) {
+    if (phoneNumber.size() != 10) return ErrorCode::Length;
     for (int i{}; i < phoneNumber.size(); ++i) {
-      if (*(phoneNumber.data() + i) < '0' && *(phoneNumber.data() + i) > '9') {
+      if (*(phoneNumber.data() + i) < '0' ||
+          *(phoneNumber.data() + i) >
+              '9') {  // Pointers for learning (date for string only)
         return ErrorCode::NoNumber;
       }
     }
